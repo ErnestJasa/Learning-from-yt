89d5e19563f3a8cfdb1c7bbbebe989f4ab39a6b3
ErnestJasa <ernestjasaitis@gmail.com>
2023-05-13 14:51:43 +0300

    blazor

---
+++ cSharp/IMS/IMS.UseCases/Interfaces/IAddInventoryUseCase.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Interfaces
+{
+    public interface IAddInventoryUseCase
+    {
+        Task EexecuteAsync(Inventory inventory);
+    }
+}
--- cSharp/IMS/InventoryManagementSystem/Program.cs
+++ cSharp/IMS/InventoryManagementSystem/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 
 builder.Services.AddTransient<IViewInventoriesByNameUseCase, ViewInventoriesByNameUseCase>();
+builder.Services.AddTransient<IAddInventoryUseCase, AddInventoryUseCase>();
+builder.Services.AddTransient<IEditInventoryUseCase, EditInventoryUseCase>();
+builder.Services.AddTransient<IViewInventoryByIdUseCase, ViewInventoryByIdUseCase>();
 
 var app = builder.Build();
 
