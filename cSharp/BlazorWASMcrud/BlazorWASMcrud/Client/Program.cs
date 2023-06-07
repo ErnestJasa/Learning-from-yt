1ffb90eaef5ce036d3f02d6d7c96bed6da655202
ErnestJasa <ernestjasaitis@gmail.com>
2023-06-07 15:18:17 +0300

    blazor webAssembly crud

---
+++ cSharp/BlazorWASMcrud/BlazorWASMcrud/Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazorWASMcrud.Client;
+using BlazorWASMcrud.Client.Services.ProductService;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IProductService, ProductService>();
+
+await builder.Build().RunAsync();
